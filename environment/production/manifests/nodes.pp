4fbb0f223c318b692a24425fc404d0e92221c599
Kennedy Alves <alvkennedy@gmail.com>
2017-02-07 13:40:42 -0200

    Adicionando coletor de recursos

--- environment/production/manifests/nodes.pp
+++ environment/production/manifests/nodes.pp
@@ -1,11 +1,17 @@
 node default {
+
 notify { 'Aplicando configurações básicas...': }
+
 include base
+
 include apache
+
 }
 
 node puppet {
+
 #Instalando os pacotes básicos para o PuppetServer
+
 package { 'puppetdb':
   ensure => present,
 }
@@ -23,7 +29,25 @@ notify { "$message":}
 # Chamando um definition type
 
 base::tmpfile { 'unset':
+
   file => [ '1', '2', '3'],
+
 }
+
+# Definindo uma tag para o node
+
+tag('tmpfile')
+
+# Validando se contém a tag tmpfile
+
+if tagged('tmpfile') {
+
+notify { 'Contém a tag tmpfile': }
+
+}
+
+# Se houver alteração em algum arquivo que contém a classe tmpfile então reinicie o serviço especificado
+
+#File <| tag == 'tmpfile' |> ~> Service['tmpfile-service']
 
 }
