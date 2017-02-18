c53a132d91648de161ff77663afb89dc125fa490
Kennedy Alves <alvkennedy@gmail.com>
2017-02-18 09:28:16 -0200

    Finalizado captura de ambiente via hostname

--- environment/production/manifests/site.pp
+++ environment/production/manifests/site.pp
@@ -3,18 +3,40 @@
 
 # Definindo ambiente através do IP
 
-if $::ipaddress =~ /192.168.*/ {
-notify { 'Ambiente de Produção': }
+#if $::ipaddress =~ /192.168.*/ {
+#notify { 'Ambiente de Produção': }
+#}
+#elsif $::ipaddress =~ /172.16.*/ {
+#notify { 'Ambiente de Stage': }
+#}
+#else {  notify { 'Não de Dev': }
+#}
+
+# Coletando o ambiente baseado no nome
+
+if $::hostname =~ /prd-*/ {
+   $ambiente = 'production'
 }
-elsif $::ipaddress =~ /172.16.*/ {
-notify { 'Ambiente de Stage': }
+elsif $::hostname =~ /stg-*/ {
+   $ambiente = 'stagging'
 }
-else {  notify { 'Não de Dev': }
+elsif $::hostanme =~ /dev-*/ {
+   $ambiente = 'dev'
+}
+else { notify { 'Ambiente não definido, aplicando o padrão': }
+   $ambiente = 'dev'
+}
+
+include base
+base::puppetconf {'unset':
+   ambiente => "$ambiente",
 }
 
 # Coletando sistema operacional
-
 if $::operatingsystem in ['CentOS'] {
+notify { 'Sistema operacional da familia RedHat': }
+}
+elsif $::operatingsystem in ['Debian','Ubuntu'] {
 notify { 'Sistema operacional da familia RedHat': }
 }
 else { notify { 'Sistema operacional da familia Debian': }
