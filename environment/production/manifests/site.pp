cf7324bfd797df5d6ee025e8ed57c0f0166cd5d5
Kennedy Alves <alvkennedy@gmail.com>
2017-02-18 16:00:55 -0200

    Limpando o código

--- environment/production/manifests/site.pp
+++ environment/production/manifests/site.pp
@@ -1,25 +1,11 @@
-## Importando arquivo nodes.pp
+# Importando arquivo nodes.pp
 #import 'nodes.pp'
-
-# Definindo ambiente através do IP
-
-#if $::ipaddress =~ /192.168.*/ {
-#notify { 'Ambiente de Produção': }
-#}
-#elsif $::ipaddress =~ /172.16.*/ {
-#notify { 'Ambiente de Stage': }
-#}
-#else {  notify { 'Não de Dev': }
-#}
-
 # Definindo um estagio para execução
 stage { 'exec_primeiro':
 before => Stage['main'],
 }
 # Criando a classe que executará o primeiro passo da receita antes dos outros
-
 class inicial {
-
 # Coletando o ambiente baseado no nome
 if $::hostname =~ /prd-*/ {
    $ambiente = 'production'
@@ -33,25 +19,15 @@ elsif $::hostanme =~ /dev-*/ {
 else { notify { 'Ambiente não definido, aplicando o padrão': }
    $ambiente = 'dev'
 }
-
 #include base
 base::puppetconf {'unset':
    ambiente => "$ambiente",
 }
-
 }
-
 # Declarando a classe criada e dizendo que ela será executada antes das demais
-
 class { 'inicial':
 stage => 'exec_primeiro',
 }
-
-# Incluindo a receita base
-
-#include base
-
 # Aplicando mensagem via hiera
-
 $message = hiera('message')
 notify { "Mensagem é $message": }
--- environment/production/modules/base/manifests/puppetconf.pp
+++ environment/production/modules/base/manifests/puppetconf.pp
@@ -1,13 +1,9 @@
 # Configurando o puppet no servidor
-
 define base::puppetconf ( $ambiente ) {
-include base
-
+#include base
 # Coletando a vrsão do Sistema operacional
-
 $get_version = "${::operatingsystemmajrelease}"
 $versao = "${get_version}"
-
 file { '/etc/puppet/puppet.conf.teste':
   ensure  => present,
   content => template('base/puppet.conf.erb'),
@@ -16,17 +12,13 @@ file { '/etc/puppet/puppet.conf.teste':
   owner   => 'puppet',
   group   => 'puppet',
 }
-
 # Criando repositório
-
 file { '/etc/yum.repos.d/puppetl.repo':
    ensure  => present,
    content => template('base/puppetl.repo.erb'),
    before  => [Package['puppet'],Service['puppet'],File['/etc/puppet/puppet.conf.teste']],
 }
-
 # Istalando e configurando o puppet agent
-
 package { 'puppet':
    ensure => installed,
    before => File['/etc/puppet/puppet.conf.teste'],
@@ -35,13 +27,9 @@ service { 'puppet':
    ensure  => running,
    require => Package['puppet'],
 }
-
 # Verificando se trata-se de um servidor puppetserver
-
 if tagged('puppetserver') {
 notify { 'Contém a tag puppetserver': }
-
 include base::puppetmaster
-
 }
 }
