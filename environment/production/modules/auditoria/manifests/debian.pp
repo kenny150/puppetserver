946a6c0d6bc7b52c4068704535629002356731ca
Kennedy Alves <alvkennedy@gmail.com>
2017-04-14 23:54:50 -0300

    Finalizando receitas de auditoria - parte 1

--- environment/production/modules/auditoria/manifests/debian.pp
+++ environment/production/modules/auditoria/manifests/debian.pp
@@ -1,5 +1,32 @@
+# Iniciando a classe 
 class auditoria::debian {
 
-notify {'Classe escolhida Debian' :}
+
+package { 'rsyslog': 
+  ensure => present,
+}
+
+
+service { 'rsyslog':
+  ensure => running,
+  require => Package['rsyslog'],
+}
+
+file { '/etc/rsyslog.d':
+  ensure => directory,
+  require => Package['rsyslog'],
+}
+
+
+file { '/etc/rsyslog.conf':
+  source => 'puppet:///modules/auditoria/rsyslog.conf.file.debian',
+  require => Package['rsyslog'],
+  notify => Service['rsyslog'],
+  mode => 0644,
+  owner => 'root',
+  group => 'root',
+}
+
+notify {'Aplicando a classe auditoria::debian' :}
 
 }
--- environment/production/modules/auditoria/manifests/redhat.pp
+++ environment/production/modules/auditoria/manifests/redhat.pp
@@ -1,5 +1,29 @@
+# Iniciando a classe 
 class auditoria::redhat {
 
-notify {'Classe escolhida RedHat' :}
+
+package { 'rsyslog': 
+  ensure => present,
+}
+
+
+service { 'rsyslog':
+  ensure => running,
+  require => Package['rsyslog'],
+}
+
+file { '/etc/rsyslog.d':
+  ensure => directory,
+  require => Package['rsyslog'],
+}
+
+
+file { '/etc/rsyslog.conf':
+  source => 'puppet:///modules/auditoria/rsyslog.conf.file.redhat'
+  require => Package['rsyslog'],
+  notify => Service['rsyslog'],
+}
+
+notify {'Aplicando a classe auditoria::reddhat' :}
 
 }
